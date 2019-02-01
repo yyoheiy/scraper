2a6f86a4cb0378c417c168a06460a8ab421a660c
yyoheiy <g.yyohiey@gmail.com>
2019-02-01 16:14:38 +0900

    start programing

--- scraper.py
+++ scraper.py
@@ -1,2 +1,11 @@
 #scraper
 #branch test
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self,site):
+        self.site=site
+    def scrape(self):
+        pass
